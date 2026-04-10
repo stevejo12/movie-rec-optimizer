783f6c1f73ca5f870d19da22880ae32b6d9e2628
Steve <steve.v.jo@gmail.com>
2026-04-10 13:11:43 -0700

    feat: gradient descent with BTLS | add: time taken for each algo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import time
 from dataloader import DataLoader
 from matrix_factorization import MatrixFactorization
-from optimizer import stochastic_gd
+from optimizer import stochastic_gd, gd_btls
+from evaluation import rmse
 
 loader = DataLoader(filepath="u.data", seed=42)
 loader.load()
@@ -8,7 +10,22 @@ loader.load()
 n_users = max(u for u, _, _ in loader.train_ratings + loader.test_ratings)
 n_items = max(m for _, m, _ in loader.train_ratings + loader.test_ratings)
 mf_model = MatrixFactorization(n_users=n_users, n_items=n_items, k=10, lam=0.01)
+U_init, V_init = mf_model.get_params()
 
-print(mf_model.get_params())
-
+mf_model.set_params(U_init, V_init)
+start_sgd = time.time()
 history = stochastic_gd(mf_model, loader.train_ratings, epochs=20)
+elapsed_sgd = time.time() - start_sgd
+
+test_rmse = rmse(mf_model, loader.test_ratings)
+print(f"Test RMSE for Stochastic Gradient Descent: {test_rmse:.4f}")
+print(f"Time taken to run Stochastic Gradient Descent: {elapsed_sgd:.2f}")
+
+mf_model.set_params(U_init, V_init)
+start_gdbtls = time.time()
+gd_btls(mf_model, loader.train_ratings, 1, 0.5, 0.1)
+elapsed_gdbtls = time.time() - start_gdbtls
+
+test_rmse = rmse(mf_model, loader.test_ratings)
+print(f"Test RMSE for Gradient Descent with BackTracking Line Search: {test_rmse:.4f}")
+print(f"Time taken to run Gradient Descent with BackTracking Line Search: {elapsed_gdbtls:.2f}")
--- optimizer.py
+++ optimizer.py
@@ -7,7 +7,7 @@ import numpy as np
 # epoch -> number of loop of iterating the data
 #   1 epoch => for example 80k data means be 80k iterations in a epoch
 # alpha_0 -> intial learning rate
-# alpha_K1 ->
+# alpha_K1 -> final learning rate
 def stochastic_gd(model, train_ratings, epochs=20, alpha_0=0.01, alpha_K1=0.001):
   history = []
   train_list = list(train_ratings)
@@ -41,3 +41,39 @@ def stochastic_gd(model, train_ratings, epochs=20, alpha_0=0.01, alpha_K1=0.001)
     print(f"Epoch {epoch+1}/{epochs}: loss = {loss:.4f}")
 
   return history
+
+# Gradient Descent + BackTracking Line Search
+# @Param
+# model: the model -> Matrix Factorization class
+# train_ratings -> the dataset for training
+# alpha -> initial step size
+# beta -> shrink factor
+# sigma -> sufficient decrease parameter
+def gd_btls(model, train_ratings: list[tuple[int, int, int]], alpha, beta, sigma):
+  history = []
+  train_list = list(train_ratings)
+  max_iter = 100
+
+  print("=== Gradient Descent with Backtracking Line Search Iteration ===")
+  alpha_k = alpha
+  for iteration in range(max_iter):
+    grad_U, grad_V = model.gradients(train_ratings) 
+    grad_norm_sq = np.sum(grad_U ** 2) + np.sum(grad_V ** 2)
+    old_loss = model.loss(train_list)
+    U_old, V_old = model.get_params()
+
+    # backtracking line search
+    while True:
+      model.U = U_old - alpha_k * grad_U
+      model.V = V_old - alpha_k * grad_V
+      new_loss = model.loss(train_list)
+
+      if new_loss <= (old_loss - sigma * alpha_k * grad_norm_sq):
+        break
+      else:
+        alpha_k *= beta
+        # revert back to the original
+        model.set_params(U_old, V_old)
+
+    history.append(new_loss)
+    print(f"Epoch {iteration+1}/{max_iter}: loss = {new_loss:.4f}")
